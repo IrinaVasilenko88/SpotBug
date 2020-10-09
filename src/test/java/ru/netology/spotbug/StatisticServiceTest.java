7d6567971ca48b943b79ef327342a717ed541a6a
Irina Vasilenko <vasilenko_i@yahoo.com>
2020-10-09 12:25:03 +0300

    Initial commit

---
+++ src/test/java/ru/netology/spotbug/StatisticServiceTest.java
@@ -0,0 +1,28 @@
+package ru.netology.spotbug;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticService service = new StatisticService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+  @Test
+    void findMaxWithAnotherArray() {
+      StatisticService service = new StatisticService();
+      long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+      long expected = 12;
+      long actual = service.findMax(incomesInBillions);
+
+      assertEquals(expected, actual);
+
+  }
+}
